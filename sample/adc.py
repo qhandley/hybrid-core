855b38c9121bf7775300dd77132087dd6d4b8703
Quinn Handley <handleyq@oregonstate.edu>
2019-10-06 13:52:14 -0700

    back to unshifted outputs

--- sample/adc.py
+++ sample/adc.py
@@ -12,11 +12,14 @@ class ADC:
     ads_reg_cnv = 0x00 # Conversion register
     ads_reg_cfg = 0x01 # Config register
 
-    def __init__(self):
+    def __init__(self, to_log):
         self.init_i2c()
-        self.log_file = self.get_ts() + ".txt"
-        f = open(self.log_file, "x")
-        f.close()
+        self.to_log = to_log
+
+        if(self.to_log == True):
+            self.log_file = self.get_ts() + ".txt"
+            f = open(self.log_file, "x")
+            f.close()
 
     def init_i2c(self):
         # Initialize i2c (SMBus)
@@ -46,7 +49,8 @@ class ADC:
         val = self.bus.read_i2c_block_data(self.i2c_adr, self.ads_reg_cnv, 2)
         result = (val[0] << 8) + val[1] # MSB + LSB
         result = (result / (2**15)) * 4.096 # Adjust for scaling
-        self.log(result)
+        if(self.to_log == True):
+            self.log(result)
         return result
 
 if __name__ == "__main__":
