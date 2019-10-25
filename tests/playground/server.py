1e37e699da5b7b4fbf6e443749185b6d25d372c6
Thomas <prihodat@oregonstate.edu>
2019-10-24 22:30:51 -0700

    file clean up

--- tests/playground/server.py
+++ tests/playground/server.py
@@ -6,85 +6,137 @@ import os
 import RPi.GPIO as GPIO
 import time
 sys.path.append("../sample/")
-import adc
+#import adc
 from socket import error as SocketError
 import errno
 
 #define variables
 CH1 = 17 #17
-CH2 = 22 #18
-CH3 = 18 #22
-CH4 = 23 #23
+CH2 = 27 #18
+CH4 = 18 #23
 
 #Configure Pins
+global Ign, Val, ERROR
+ERROR = "0"
 GPIO.setwarnings(False) #silence setup warnings
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CH1, GPIO.IN)
 GPIO.setup(CH2, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(CH3, GPIO.IN)
+Ign = "0"
 GPIO.setup(CH4, GPIO.OUT, initial=GPIO.HIGH)
+Val = "0"
 
 #Reading/logging adc values
 #adc = adc.ADC(True)
 
-#Reset IO
 def reset():
-    csocket.send(bytes("Resetting",'utf-8'))
+    global msg, Ign, Val
+    msg = "Resetting"
     GPIO.output(CH2, GPIO.HIGH)
+    Ign = "0"
     GPIO.output(CH4, GPIO.HIGH)
+    Val = "0"
     print("resetting")
 
-#Handle Signals
-def SIG_Handler(sig, frame):
+def SIG_ALRM_Handler(sig, frame):
+    global ERROR
+    print("ERROR: Connection Timeout")
+    ERROR = "ERROR: Connection Timeout"
     reset()
-    print("\nExitting Safely")
-    global server
-    #close socket
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
+    exit()
+    #raise ValueError
+
+def SIG_USR_Handler(sig, frame):
+    print("Aborting")
+    msg ="Aborting"
+    reset()
+    raise ValueError
+
+def SIG_Handler(sig, frame):
+    global server,csocket
+    reset()
+    #print("Exitting Safely")
     server.shutdown(socket.SHUT_RDWR)
     server.close()
     exit()
 
-def SIG_USR_Handler(sig, frame):
-    raise ValueError
-    
-#Function used for server thread
 def server_thread():
-    server.listen(1)
-    clientsocket, clientAddress = server.accept()
-    global csocket
-    global msg
-    global command
-    csocket = clientsocket
-    print("Connection Established")
-    print("Connection from : ", clientAddress)
-    print(csocket.recv(1024))
-    csocket.send(bytes("What up", 'utf-8'))
-    msg = "0"
-    while True:
-        data = csocket.recv(2048)
-        data = data.decode()
-        if data == 'a':
-            os.kill(os.getpid(),signal.SIGUSR1)
-            break
-        elif data == 'kill':
-            os.kill(os.getpid(),signal.SIGINT)
-            exit()
-        elif data != "0":
-            print(data)
-            command = data
-            data = "0"
-        csocket.send(bytes(msg,'utf-8'))
+    global threadID
+    threadID = threading.get_ident()
+    try:
+        server.listen(1)
+        clientsocket, clientAddress = server.accept()
+        global csocket
+        global msg
+        global command
+        csocket = clientsocket
+        print("Connection Established")
+        print("Connection from : ", clientAddress)
+        print(csocket.recv(1024))
         msg = "0"
+        while True:
+            signal.alarm(3)
+            data = csocket.recv(1024)
+            signal.alarm(0)
+            data = data.decode()
+            data,ignore1,ignore2 = data.partition("0")
+            if data == 'a':
+                os.kill(os.getpid(), signal.SIGUSR1)
+            elif data == 'kill':
+                os.kill(os.getpid(), signal.SIGINT)
+                break
+            elif data != "":
+                print(data)
+                command = data
+                data = "0"
+            if GPIO.input(CH1) == GPIO.HIGH:
+                Burn = "0"
+            else:
+                Burn = "1"
+            Pre = str(round(12.12165135, 1))
+            #Pre = str(round(adc.read(), 1))
+            data_out =""
+            data_out = msg +";"+ Ign +";"+ Val +";"+ Burn +";"+ Pre +";" + ERROR + ";"
+            #print(data_out)
+            csocket.send(bytes(data_out,'utf-8'))
+    except SocketError:
+        print("EXCEPTION")
+        os.kill(os.getpid(), signal.SIGINT)
     print("Connection Broken")
+    threadID = 0
 
 def Command_Response(Command):
-    global msg
+    global msg, Ign, Val, ERROR
     while True:
         if Command == "1":
+            reset()
             if GPIO.input(CH1) == GPIO.HIGH:
-                msg = "ERROR: Burn wire cut"
+                ERROR = "ERROR: Burn wire cut"
                 print("ERROR: Burn wire cut")
                 return
+            msg= "Ten"
+            print("Ten")
+            time.sleep(1)
+            msg= "Nine"
+            print("Nine")
+            time.sleep(1)
+            msg= "Eight"
+            print("Eight")
+            time.sleep(1)
+            msg= "Seven"
+            print("Seven")
+            time.sleep(1)
+            msg= "Six"
+            print("Six")
+            time.sleep(1)
+            msg= "Five"
+            print("Five")
+            time.sleep(1)
+            msg= "Four"
+            print("Four")
+            time.sleep(1)
             msg= "Three"
             print("Three")
             time.sleep(1)
@@ -100,17 +152,20 @@ def Command_Response(Command):
             while GPIO.input(CH1) == GPIO.LOW:
                 if time.perf_counter() - start_time < 10:
                     GPIO.output(CH2, GPIO.LOW)
+                    Ign = "1"
                 else:
-                    msg = "ERROR: Ignition timeout"
+                    ERROR = "ERROR: Ignition timeout"
                     print("ERROR: Ignition timeout")
                     reset()
                     return
             msg = "Stop Ignition"
             print("Stop Ignition")
             GPIO.output(CH2, GPIO.HIGH)
+            Ign = "0"
             msg = "Opening the Valve"
             print("Opening the Valve")
             GPIO.output(CH4, GPIO.LOW)
+            Val = "1"
 
             msg = "Waiting for pressure build"
             print("Waiting for pressure build")
@@ -124,6 +179,7 @@ def Command_Response(Command):
             msg = "Closing the Valve"
             print("Closing the Valve")
             GPIO.output(CH4, GPIO.HIGH)
+            Val = "0"
             msg = "Ignition Sequence Complete"
             print("Ignition Sequence Complete")
             break
@@ -132,65 +188,60 @@ def Command_Response(Command):
             msg = "Ignition ON"
             print("Ignition ON")
             GPIO.output(CH2, GPIO.LOW)
+            Ign = "1"
             break
 
         elif Command == "3":
             msg = "Ignition OFF"
             print("Ignition OFF")
             GPIO.output(CH2, GPIO.HIGH)
+            Ign = "0"
             break
 
         elif Command == "4":
             msg = "Valve OPEN"
             print("Valve OPEN")
             GPIO.output(CH4, GPIO.LOW)
+            Val = "1"
             break
 
         elif Command == "5":
             msg = "Valve CLOSE"
             print("Valve CLOSE")
             GPIO.output(CH4, GPIO.HIGH)
+            Val = "0"
             break
         else:
-            msg = "ERROR invalid input"
+            ERROR = "ERROR invalid input"
             print("ERROR invalid input")
             break
 
     
-#Initialze Signal Handlers    
+    
 signal.signal(signal.SIGPIPE, SIG_Handler)
+signal.signal(signal.SIGALRM, SIG_ALRM_Handler)
 signal.signal(signal.SIGINT, SIG_Handler)
-#Define IP and Port
+signal.signal(signal.SIGUSR1, SIG_USR_Handler)
+
 LOCALHOST = "192.168.0.10"
 PORT = 8080
-#Create Server Socket using IPV4 and TCP
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Add Socket Options: reuse Socket
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((LOCALHOST, PORT))
 print("server started")
-#command used to share messages from client between threads
-global command
-#Keep Connecting using Socket as long as program running
+global command, threadID
+threadID = 0
+command = "0"
 while True:
-    print("Waiting for client request...")
-    data ="0"
-    try:
-        thread = threading.Thread(target = server_thread(), daemon = True)
+    if threadID == 0:
+        print("Waiting for client request...")
+        thread = threading.Thread(target = server_thread, daemon = True)
         thread.start()
-    except SocketError:
-        print("EXCEPTION")
-        GPIO.output(CH2, GPIO.HIGH)
-        GPIO.output(CH4, GPIO.HIGH)
-        print("Resetting")
-    while True:
-        if command != "0":
-            try:
+    try:
+        while True:
+            if command != "0":
                 Command_Response(command)
-            except:
-                reset()
-                break
-            finally:
                 command = "0"
-        else:
-            pass
+    except ValueError:
+        command = "0"
+
