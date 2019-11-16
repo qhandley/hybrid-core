495e30aaf6d2d8b315e9b2d2dc3e69514c49f953
Thomas <prihodat@oregonstate.edu>
2019-11-16 11:46:14 -0800

    updateded test server

--- tests/test_server.py
+++ tests/test_server.py
@@ -27,11 +27,36 @@ GPIO.setup(CH4, GPIO.OUT, initial=GPIO.HIGH)
 Val = "0"
 
 #Reading/logging adc values
-adc = adc.ADC(True)
+#adc = adc.ADC(True)
+def setMessage(message):
+    global msg
+    global current
+    bound = 4
+    if msg[current] == "0":
+        msg[current] = message
+    elif current < bound:
+        current = current + 1
+        msg[current] = message
+
+
+def getMessage():
+    global msg
+    global current
+    zero = "0"
+    message = msg[0]
+    print(current)
+    msg[0] = zero
+    if current > 0:
+        for i in range(current):
+            msg[i] = msg[i+1]
+        current -= 1
+    return message
+
+
 
 def reset():
     global msg, Ign, Val
-    msg = "Resetting"
+    setMessage("Resetting")
     GPIO.output(CH2, GPIO.HIGH)
     Ign = "0"
     GPIO.output(CH4, GPIO.HIGH)
@@ -50,7 +75,7 @@ def SIG_ALRM_Handler(sig, frame):
 
 def SIG_USR_Handler(sig, frame):
     print("Aborting")
-    msg ="Aborting"
+    setMessage("Aborting")
     reset()
     raise ValueError
 
@@ -61,26 +86,6 @@ def SIG_Handler(sig, frame):
     server.shutdown(socket.SHUT_RDWR)
     server.close()
     exit()
-def setMessage(message):
-    global msg
-    global current
-    bound = 4
-    if msg[current] == "0":
-        msg[current] = message
-    elif current < bound:
-        current = current + 1
-        msg[current] = message
-
-
-def getMessage(message):
-    global msg
-    global current
-    message = msg[current]
-    msg[current] = "0"
-    if current > 0:
-        current -= 1
-    return message
-
 
 def server_thread():
     global threadID
@@ -91,6 +96,8 @@ def server_thread():
         global csocket
         global command
         global msg
+        global current
+        current = 0
         csocket = clientsocket
         print("Connection Established")
         print("Connection from : ", clientAddress)
@@ -115,12 +122,12 @@ def server_thread():
                 Burn = "0"
             else:
                 Burn = "1"
-            #Pre = str(round(12.12165135, 1))
-            Pre = str(round(adc.read(), 1))
+            Pre = str(round(12.12165135, 1))
+#            Pre = str(round(adc.read(), 1))
             message = getMessage()
             data_out =""
             data_out = message +";"+ Ign +";"+ Val +";"+ Burn +";"+ Pre +";" + ERROR + ";"
-            #print(data_out)
+            print(data_out)
             csocket.send(bytes(data_out,'utf-8'))
     except SocketError:
         print("EXCEPTION")
@@ -130,118 +137,113 @@ def server_thread():
 
 def Command_Response(Command):
     global msg, Ign, Val, ERROR
-    while True:
-        if Command == "1":
-            reset()
-            if GPIO.input(CH1) == GPIO.HIGH:
-                ERROR = "ERROR: Burn wire cut"
-                print("ERROR: Burn wire cut")
+    if Command == "1":
+        reset()
+        if GPIO.input(CH1) == GPIO.HIGH:
+            ERROR = "ERROR: Burn wire cut"
+            print("ERROR: Burn wire cut")
+            return
+        setMessage("Ten")
+        print("Ten")
+        time.sleep(1)
+        setMessage("Nine")
+        print("Nine")
+        time.sleep(1)
+        setMessage("Eight")
+        print("Eight")
+        time.sleep(1)
+        setMessage("Seven")
+        print("Seven")
+        time.sleep(1)
+        setMessage("Six")
+        print("Six")
+        time.sleep(1)
+        setMessage("Five")
+        print("Five")
+        time.sleep(1)
+        setMessage("Four")
+        print("Four")
+        time.sleep(1)
+        setMessage("Three")
+        print("Three")
+        time.sleep(1)
+        setMessage("Two")
+        print("Two")
+        time.sleep(1)
+        setMessage("One")
+        print("One")
+        time.sleep(1)
+        start_time = time.perf_counter()
+        setMessage("Start Ignition")
+        print("Start Ignition")
+        while GPIO.input(CH1) == GPIO.LOW:
+            if time.perf_counter() - start_time < 10:
+                GPIO.output(CH2, GPIO.LOW)
+                Ign = "1"
+            else:
+                ERROR = "ERROR: Ignition timeout"
+                print("ERROR: Ignition timeout")
+                reset()
                 return
-            msg= "Ten"
-            print("Ten")
-            time.sleep(1)
-            msg= "Nine"
-            print("Nine")
-            time.sleep(1)
-            msg= "Eight"
-            print("Eight")
-            time.sleep(1)
-            msg= "Seven"
-            print("Seven")
-            time.sleep(1)
-            msg= "Six"
-            print("Six")
-            time.sleep(1)
-            msg= "Five"
-            print("Five")
-            time.sleep(1)
-            msg= "Four"
-            print("Four")
-            time.sleep(1)
-            msg= "Three"
-            print("Three")
-            time.sleep(1)
-            msg = "Two"
-            print("Two")
-            time.sleep(1)
-            msg = "One"
-            print("One")
-            time.sleep(1)
-            start_time = time.perf_counter()
-            msg = "Start Ignition"
-            print("Start Ignition")
-            while GPIO.input(CH1) == GPIO.LOW:
-                if time.perf_counter() - start_time < 10:
-                    GPIO.output(CH2, GPIO.LOW)
-                    Ign = "1"
-                else:
-                    ERROR = "ERROR: Ignition timeout"
-                    print("ERROR: Ignition timeout")
-                    reset()
-                    return
-            msg = "Stop Ignition"
-            print("Stop Ignition")
-            GPIO.output(CH2, GPIO.HIGH)
-            Ign = "0"
-            msg = "Opening the Valve"
-            print("Opening the Valve")
-            GPIO.output(CH4, GPIO.LOW)
-            Val = "1"
+        setMessage("Stop Ignition")
+        print("Stop Ignition")
+        GPIO.output(CH2, GPIO.HIGH)
+        Ign = "0"
+        setMessage("Opening the Valve")
+        print("Opening the Valve")
+        GPIO.output(CH4, GPIO.LOW)
+        Val = "1"
 
-            msg = "Waiting for pressure build"
-            print("Waiting for pressure build")
-            adc.set_ref_time()
-            while adc.read() < 100:
-                pass
-            msg = "Waiting for pressure drop"
-            print("Waiting for pressure drop")
-            temp = adc.read()
-            while temp > 70 and temp < 900:
-                temp = adc.read()
-                pass
-            msg = "Closing the Valve"
-            print("Closing the Valve")
-            GPIO.output(CH4, GPIO.HIGH)
-            Val = "0"
-            msg = "Ignition Sequence Complete"
-            print("Ignition Sequence Complete")
-            break
+        setMessage("Waiting for pressure build")
+        print("Waiting for pressure build")
+        #adc.set_ref_time()
+        while True: #adc.read() < 100:
+            pass
+        setMessage("Waiting for pressure drop")
+        print("Waiting for pressure drop")
+        #temp = adc.read()
+        while temp > 70 and temp < 900:
+            #temp = adc.read()
+            pass
+        setMessage("Closing the Valve")
+        print("Closing the Valve")
+        GPIO.output(CH4, GPIO.HIGH)
+        Val = "0"
+        setMessage("Ignition Sequence Complete")
+        print("Ignition Sequence Complete")
+        return
 
-        elif Command == "2":
-            msg = "Ignition ON"
-            setMessage("1")
-            setMessage("2")
-            setMessage("3")
-            setMessage("4")
-            print("Ignition ON")
-            GPIO.output(CH2, GPIO.LOW)
-            Ign = "1"
-            break
+    elif Command == "2":
+        setMessage("Ignition ON")
+        print("Ignition ON")
+        GPIO.output(CH2, GPIO.LOW)
+        Ign = "1"
+        return
 
-        elif Command == "3":
-            msg = "Ignition OFF"
-            print("Ignition OFF")
-            GPIO.output(CH2, GPIO.HIGH)
-            Ign = "0"
-            break
+    elif Command == "3":
+        setMessage("Ignition OFF")
+        print("Ignition OFF")
+        GPIO.output(CH2, GPIO.HIGH)
+        Ign = "0"
+        return
 
-        elif Command == "4":
-            msg = "Valve OPEN"
-            print("Valve OPEN")
-            GPIO.output(CH4, GPIO.LOW)
-            Val = "1"
-            break
+    elif Command == "4":
+        setMessage("Valve OPEN")
+        print("Valve OPEN")
+        GPIO.output(CH4, GPIO.LOW)
+        Val = "1"
+        return
 
-        elif Command == "5":
-            msg = "Valve CLOSE"
-            print("Valve CLOSE")
-            GPIO.output(CH4, GPIO.HIGH)
-            Val = "0"
-            break
-        else:
-            ERROR = "ERROR invalid input"
-            print("ERROR invalid input")
-            break
+    elif Command == "5":
+        setMessage("Valve CLOSE")
+        print("Valve CLOSE")
+        GPIO.output(CH4, GPIO.HIGH)
+        Val = "0"
+        return
+    else:
+        ERROR = "ERROR invalid input"
+        print("ERROR invalid input")
+        return
 
     
     
@@ -250,7 +252,7 @@ signal.signal(signal.SIGALRM, SIG_ALRM_Handler)
 signal.signal(signal.SIGINT, SIG_Handler)
 signal.signal(signal.SIGUSR1, SIG_USR_Handler)
 
-LOCALHOST = "192.168.1.10"
+LOCALHOST = "192.168.0.10"
 PORT = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
